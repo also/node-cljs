2ab5c069d4d2a85165c28ad9ac267333f7b2898b
Ryan Berdeen <ryan@ryanberdeen.com>
2013-11-24 23:18:20 -0500

    don't fail when cljs.core isn't exported

--- command.js
+++ command.js
@@ -4,6 +4,10 @@ var oogCommand = require('oog/command');
 
 var opts = oogCommand.parseArgs();
 var goog = oogCommand.init(opts);
-var cljs = goog.require('cljs.core');
-cljs.enable_console_print_BANG_();
+try {
+  var cljs = goog.require('cljs.core');
+  cljs.enable_console_print_BANG_();
+} catch (e) {
+  console.warn(e.message);
+}
 oogCommand.run(opts, goog);
